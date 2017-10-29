9539a896404df4fd96cb251b5ba463f7cbc4fdba
Rowen Wu <kathertw@usc.edu>
2017-10-28 18:24:20 -0700

    started front end

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('search');
+  this.route('saved');
 });
 
 export default Router;
---
+++ tests/unit/routes/saved-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:saved', 'Unit | Route | saved', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
