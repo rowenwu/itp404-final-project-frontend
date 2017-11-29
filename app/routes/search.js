0df806cd3fe5ff4dfc5602ecb6b4fbee424be0fc
Rowen Wu <kathertw@usc.edu>
2017-11-28 20:26:00 -0800

    changed ajax to jquery

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
-  ajax: Ember.inject.service(),  
+  // ajax: Ember.inject.service(),  
   
   queryParams: {
     keywords: {
@@ -13,21 +14,27 @@ export default Ember.Route.extend({
   },
   model(params) {
     if(params.location != null){
-      let promise = this.get('ajax').request('/venues/explore', {
-        method: 'GET',
-        data: {
-          client_id: 'MSVU5SD1IF33JNIPQGEYRLKNPHL3OQYIZFWPOAKACIQNK23L',
-          client_secret: 'J5AIPR0520VJDBPYB4HRL0ZW3F22PDQKUCPHBQI2JKN43MC3',
-          near: params.location,
-          // ll: lat + ',' + long,
-          // ll: '40.7243,-74.0018',
+
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.foursquare.com/v2/venues/explore',
+      data: {
+        client_id: 'MSVU5SD1IF33JNIPQGEYRLKNPHL3OQYIZFWPOAKACIQNK23L',
+        client_secret: 'J5AIPR0520VJDBPYB4HRL0ZW3F22PDQKUCPHBQI2JKN43MC3',
+        near: params.location,
+        // ll: lat + ',' + long,
+        // ll: '40.7243,-74.0018',
+        
+        query: params.keywords,
+        v: '20170801',
+        limit: 5
           
-          query: params.keywords,
-          v: '20170801',
-          limit: 5
-            
-        }
-      });
-      console.log(promise);    }    
+      }
+    }).then(function(result) {
+      console.log(result.response.groups[0].items);
+      return result.response.groups[0].items;
+    });
+
+     }    
   }
 });
